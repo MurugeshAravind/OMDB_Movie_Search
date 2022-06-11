1a67c23c9fdcd823280b169b68c5b7d60b6e51e0
Mallappan Balasubramaniam <mmallappan@luxoft.com>
2022-06-11 21:21:01 +0530

    changes made in the App.js and package.json

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
       <Fragment>
         <Routes>
-          <Route path="/" element={<MovieList />} />
+          <Route exact path="/" element={<MovieList />} />
           <Route path="/:id" element={<MovieDetails />} />
         </Routes>
       </Fragment>
